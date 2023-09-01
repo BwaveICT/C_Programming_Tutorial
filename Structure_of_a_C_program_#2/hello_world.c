e8d954a4e88c8c948a8c4164eea690cb061b7b0e
BRIGHT (Bwave) <ivquestservices@gmail.com>
2023-09-01 09:31:35 +0100

    Structure of a c program

---
+++ Structure_of_a_C_program_#2/hello_world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>    // Header File
+
+int main() //main function
+{
+    printf("Hello Bwave \n");  //printf function
+    
+
+    return 0;  //return statement
+}
+
+
+// This is a single line comment
+
+
+/*
+    this is a multiline comment
+    We can comment multiplie lines.
+    
+*/
